005d4e11c0a996d70703da14bfcb959a8c473283
Damien Senger <damien@raccoon.studio>
2019-12-21 15:27:12 +0100

    Update TabButton border using pseudo element
    Changes the system used to display a thicker bottom border for the
    active `TabButton` used by the `Tabs` component.
    
    This component is now using a pseudo element to display and animate
    a thicker border when the tab is active without changing the size
    of the box, and limiting the impact on all other tabs.

--- src/components/Tabs/components/TabButton.ts
+++ src/components/Tabs/components/TabButton.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 
 export default styled.button(
   ({ theme }) => css`
+    position: relative;
     flex: 1;
     padding: 1.2rem 1.6rem;
     background: none;
@@ -10,13 +11,30 @@ export default styled.button(
     font-size: 1.8rem;
     font-weight: 350;
 
+    &::after {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 100%;
+      content: '';
+      height: 0.1rem;
+      background: ${theme.palette.primary};
+    }
+
     &[aria-expanded='true'] {
-      border-bottom-width: 0.3rem;
       font-weight: 650;
+
+      &::after {
+        height: 0.3rem;
+      }
     }
 
     @media (prefers-reduced-motion: no-preference) {
-      transition: border-width 0.3s ease-in-out, font-weight 0.3s ease-in-out;
+      transition: font-weight 0.3s ease-in-out;
+
+      &::after {
+        transition: height 0.3s ease-in-out;
+      }
     }
   `,
 );

e5d6cf1cc1aab342d78183d03f66546a976a3a9e
Damien Senger <damien@raccoon.studio>
2019-12-21 14:07:51 +0100

    Implement ThemeProvider in BaseStyles
    This commit implements the `ThemeProvider` from the
    `styled-components` library as part of the `BaseStyles` component
    to share the application's theme with all styled components.
    
    To share this theme with all styled components, this commit also
    implements a type definition file to override the default one from
    `styled-components` to extend the `DefaultTheme` with the typings
    from our own `Theme` interface.
    
    References:
    
     - https://github.com/styled-components/styled-components/issues/1589#issuecomment-456641381
     - https://github.com/castoredc/design-system/blob/bf9fb1d4f918afd0b463163c5406583f857ae8c8/src/typings/styled-components.d.ts

--- src/components/BaseStyles/BaseStyles.tsx
+++ src/components/BaseStyles/BaseStyles.tsx
@@ -1,11 +1,15 @@
 import React, { FunctionComponent } from 'react';
+import { ThemeProvider } from 'styled-components';
+import themes from '../../themes';
 import { FontLoader } from '../FontLoader';
 import { GlobalStyles } from './components';
 
 export const BaseStyles: FunctionComponent = ({ children }) => (
-  <>
-    <FontLoader />
-    <GlobalStyles />
-    {children}
-  </>
+  <ThemeProvider theme={themes.default}>
+    <>
+      <FontLoader />
+      <GlobalStyles />
+      {children}
+    </>
+  </ThemeProvider>
 );

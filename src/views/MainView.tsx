e4a66a705867ee3de7ce0e08eb9efdbcb503ebf1
Damien Senger <damien@raccoon.studio>
2019-12-22 16:17:45 +0100

    Use react-helmet to manage the title and head info
    This commit adds `react-helmet` as a dependency of this project in
    order to manage the content of the `<head />` section of the page.
    
    This commit uses this new package to manage and updates the title
    of the page (Cerberus during loading then name of the project) and
    updates the `meta` element containing the `theme-color` attribute
    to use the `tertiary` color of our theme.

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
+import { Helmet } from 'react-helmet';
+import { ThemeContext } from 'styled-components';
 import { useQuery } from '@apollo/react-hooks';
 import { RepositoryData, RepositoryQueryVars } from '../data/repository/types';
 import {
@@ -15,6 +17,7 @@ import { repositoryQuery } from '../data';
  * Defines the content for the main view of the Cerberus application
  */
 const MainView: FunctionComponent = () => {
+  const theme = useContext(ThemeContext);
   const repositoryQueryVars = useRepository();
 
   // fetches all information about the repository
@@ -33,24 +36,30 @@ const MainView: FunctionComponent = () => {
   const { repository } = data;
   const { deployments, pullRequests } = repository;
 
+  // prepares the title used for this view
+  const viewTitle = `Latest deployments for ${repository.owner.login}/${repository.name}`;
+
   return (
-    <Layout>
-      <Header>
-        Latest deployments for {repository.owner.login}/{repository.name}
-      </Header>
-      <Tabs
-        tabs={{
-          pullRequests: {
-            content: <PullRequestList list={pullRequests} />,
-            label: 'Open pull requests',
-          },
-          deployments: {
-            content: <DeploymentList list={deployments} />,
-            label: 'Latest deployments',
-          },
-        }}
-      />
-    </Layout>
+    <>
+      <Helmet title={`${viewTitle} by Cerberus`}>
+        <meta name="theme-color" content={theme.palette.tertiary} />
+      </Helmet>
+      <Layout>
+        <Header>{viewTitle}</Header>
+        <Tabs
+          tabs={{
+            pullRequests: {
+              content: <PullRequestList list={pullRequests} />,
+              label: 'Open pull requests',
+            },
+            deployments: {
+              content: <DeploymentList list={deployments} />,
+              label: 'Latest deployments',
+            },
+          }}
+        />
+      </Layout>
+    </>
   );
 };
 
